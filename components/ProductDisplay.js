b357bb2bf2c55791a47221d9b15cea8b5d0a0c1f
chloegroff <73137978+chloegroff@users.noreply.github.com>
2023-04-18 13:56:18 +0100

    lesson 10 not done

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -32,12 +32,19 @@ app.component('product-display', {
           :style="{ backgroundColor: variant.color }">
         </div>
         
-        <button 
+        <button
           class="button" 
           :class="{ disabledButton: !inStock }" 
           :disabled="!inStock" 
           v-on:click="addToCart">
           Add to Cart
+        </button>
+        <button
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove from Cart
         </button>
       </div>
     </div>
@@ -56,11 +63,14 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
-      },
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+    },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+    },
       updateVariant(index) {
-          this.selectedVariant = index
-      }
+        this.selectedVariant = index
+    }
   },
   computed: {
       title() {
